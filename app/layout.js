1a9c1ee34035bb76fe2678aa4e1313cf4f5e2dec
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2024-10-12 09:06:55 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Miguel's Calculator: Calculadora de Algebra",
-  description: "Uma calculadora de algebra feita com integração com o GPT-4o para resoluções com explicação",
+  description: "Uma calculadora de algebra feita com integração com o GPT-4o para resoluções com explicações passo a passo.",
 };
 
 export default function RootLayout({ children }) {
